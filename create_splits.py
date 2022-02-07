2b06062595733dd4f602adb98a1c137bd75dcc5d
kaka <vn503024@gmail.com>
2022-02-07 10:33:27 +0800

    feat: completed the code of splitting dataset

--- create_splits.py
+++ create_splits.py
@@ -1,7 +1,7 @@
 import argparse
 import glob
 import os
-import random
+import shutil
 
 import numpy as np
 
@@ -18,6 +18,38 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     # TODO: Implement function
+
+    # Create train/val/test directories
+    train_dir = os.path.join(destination, "train")
+    val_dir = os.path.join(destination, "val")
+    test_dir = os.path.join(destination, "test")
+
+    try:
+        os.mkdir(train_dir)
+        os.mkdir(val_dir)
+        os.mkdir(test_dir)
+    except OSError as error:
+        # print(error)
+        pass
+
+    # Get list of data to use
+    record_files = [filename for filename in glob.glob(f'{source}/*.tfrecord')]
+    if len(record_files) == 0:
+        print("No data to split, Exit")
+        return
+    np.random.shuffle(record_files)
+
+    # Split dataset
+    train_files, val_files, test_files = np.split(record_files, [int(.75 * len(record_files)), int(.9 * len(record_files))])
+
+    for file in train_files:
+        shutil.move(file, train_dir)
+
+    for file in val_files:
+        shutil.move(file, val_dir)
+
+    for file in test_files:
+        shutil.move(file, test_dir)
 
 
 if __name__ == "__main__":
